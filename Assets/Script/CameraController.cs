1e687136eabb5ff6b6aff23cbe3a9fa1e92877b9
Gnaht <rexming2016@gmail.com>
2024-10-02 12:12:07 +0700

    Update RoomCamera
    CAMERA MOVEMENT

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Update()
+    {
+        //Gradually changes a vector towards a desired goal over time.
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z),
+            ref velocity, speed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
