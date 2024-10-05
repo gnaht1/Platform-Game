c0b417f3d1adee60dad1c40c3a7ac5adaf8e3388
Vietzzzz <doanhviet7777@gmail.com>
2024-10-05 18:10:27 +0700

    fix jump

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,83 +1,59 @@
 ﻿using UnityEngine;
 
-public class PlayerControllerSimple : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
-    [SerializeField] private LayerMask groundLayer; // Boxcast
+    [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
-    // Thành phần Animator
-    private Animator anim;
-    // Thành phần Rigidbody2D
     private Rigidbody2D body;
-    // Thành phần Boxcollider2D
+    private Animator anim;
     private BoxCollider2D boxCollider;
-    //Create delay for each wall jump
     private float wallJumpCooldown;
     private float horizontalInput;
 
-
-    void Awake()
+    private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    void Update()
+    private void Update()
     {
-
         horizontalInput = Input.GetAxis("Horizontal");
 
-        // Thay đổi hướng của nhân vật mà không thay đổi chiều cao (y) hoặc độ sâu (z)
+        //Flip player when moving left-right
         if (horizontalInput > 0.01f)
-        {
-            // Đảm bảo nhân vật luôn nhìn về bên phải (x là số dương)
             transform.localScale = Vector3.one;
-        }
         else if (horizontalInput < -0.01f)
-        {
-            // Đảm bảo nhân vật luôn nhìn về bên trái (x là số âm)
             transform.localScale = new Vector3(-1, 1, 1);
-        }
 
+        //Set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", isGrounded());
 
         //Wall jump logic
-        if (wallJumpCooldown > 0.2f)
+        if (wallJumpCooldown > 0.25f)
         {
-
             body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
             if (onWall() && !isGrounded())
             {
-                //If the player jump on a wall, he will get stuck and not be able to fall down
-                body.gravityScale = 0;
+                body.gravityScale = 1;
                 body.velocity = Vector2.zero;
-
             }
             else
-            {
                 body.gravityScale = 3;
-            }
 
-            if (Input.GetKey(KeyCode.Space) && isGrounded()) // allow player to jump only if grounded
-            {
+            if (Input.GetKey(KeyCode.Space))
                 Jump();
-            }
         }
         else
-        {
             wallJumpCooldown += Time.deltaTime;
-        }
-
-        anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", isGrounded());
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-    }
     private void Jump()
     {
         if (isGrounded())
@@ -87,23 +63,13 @@ public class PlayerControllerSimple : MonoBehaviour
         }
         else if (onWall() && !isGrounded())
         {
-            if (horizontalInput == 0)
-            {
-                //10: the power which the player will be pushed away from the wall
-                //0: the power which the player will be pushed up
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
-                //flip the player
-                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-            else
-            {
-
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
-            }
-
+            body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x), 4.3f);
             wallJumpCooldown = 0;
         }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
     }
 
     private bool isGrounded()
@@ -111,10 +77,9 @@ public class PlayerControllerSimple : MonoBehaviour
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         return raycastHit.collider != null;
     }
-
     private bool onWall()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.05f, wallLayer);
         return raycastHit.collider != null;
     }
 }
