495497821e3cbcbe9c360bb0c1a778db29989c91
Gnaht <rexming2016@gmail.com>
2024-10-06 21:47:13 +0700

    Change Camera
    Follow player camera

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //Room camera
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    //Follow player camera
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+
     private void Update()
     {
+        //Room camera
         //Gradually changes a vector towards a desired goal over time.
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z),
-            ref velocity, speed);
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z),
+        //    ref velocity, speed);
+
+
+        //Follow player camera
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+
+
     }
 
     public void MoveToNewRoom(Transform _newRoom)
