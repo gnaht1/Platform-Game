d8ae441584a2235de8f00393bd18cbc8ca03ab07
Vietzzzz <doanhviet7777@gmail.com>
2024-11-13 10:57:32 +0700

    update checkpoint and respawn

--- Assets/Script/Health/Health.cs
+++ Assets/Script/Health/Health.cs
@@ -1,5 +1,5 @@
-using System.Collections;
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
@@ -10,69 +10,84 @@ public class Health : MonoBehaviour
     private bool dead;
 
     [Header("iFrames")]
-    [SerializeField] private float iFramesDuration; // Time that player is invulnerable 
-    [SerializeField] private int numberOfFlashes; // Number of times the player flashes red
-    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+    private bool invulnerable;
 
     [Header("Death Sound")]
     [SerializeField] private AudioClip deathSound;
     [SerializeField] private AudioClip hurtSound;
 
-
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
-
     public void TakeDamage(float _damage)
     {
-        // Limit health
+        if (invulnerable) return;
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
-        
+
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
-            StartCoroutine(Invulerability());
-
+            StartCoroutine(Invunerability());
             SoundManager.instance.PlaySound(hurtSound);
         }
         else
         {
             if (!dead)
             {
-                GetComponent<PlayerMovement>().enabled = false;
+                //Deactivate all attached component classes
+                foreach (Behaviour component in components)
+                    component.enabled = false;
 
                 anim.SetBool("grounded", true);
                 anim.SetTrigger("die");
 
-                dead = true; 
-
+                dead = true;
                 SoundManager.instance.PlaySound(deathSound);
-
             }
-            
         }
     }
-
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
-
-    private IEnumerator Invulerability()
+    private IEnumerator Invunerability()
     {
+        invulnerable = true;
         Physics2D.IgnoreLayerCollision(10, 11, true);
         for (int i = 0; i < numberOfFlashes; i++)
         {
-
-            spriteRenderer.color = new Color(1, 0, 0, 0.5f);
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
             yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
-            spriteRenderer.color = Color.white;
+            spriteRend.color = Color.white;
             yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
         Physics2D.IgnoreLayerCollision(10, 11, false);
+        invulnerable = false;
+    }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
 
+    //Respawn
+    public void Respawn()
+    {
+        AddHealth(startingHealth);
+        anim.ResetTrigger("die");
+        anim.Play("Idle");
+        StartCoroutine(Invunerability());
+
+        //Activate all attached component classes
+        foreach (Behaviour component in components)
+            component.enabled = true;
     }
 }
