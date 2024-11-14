822eb102f19817e648ebbe69bb69ece18a212d2d
Gnaht <rexming2016@gmail.com>
2024-11-14 11:31:00 +0700

    Start Menu
     still beta :+1:

---
+++ Assets/Script/Menu/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
