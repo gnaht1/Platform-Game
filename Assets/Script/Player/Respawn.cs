a3294280ad5a3c09cd032856a5e253e6c2bd6281
Vietzzzz <doanhviet7777@gmail.com>
2024-11-27 15:15:57 +0700

    update pause

--- Assets/Script/Player/Respawn.cs
+++ Assets/Script/Player/Respawn.cs
@@ -5,14 +5,21 @@ public class PlayerRespawn : MonoBehaviour
     [SerializeField] private AudioClip checkpoint;
     private Transform currentCheckpoint;
     private Health playerHealth;
+    private UIManager uiManager;
 
     private void Awake()
     {
         playerHealth = GetComponent<Health>();
+        uiManager = FindObjectOfType<UIManager>();
     }
 
-    public void Respawn()
+    public void CheckRespawn()
     {
+        if (currentCheckpoint == null) { 
+            uiManager.GameOver();
+            return;
+        }
+
         playerHealth.Respawn(); //Restore player health and reset animation
         transform.position = currentCheckpoint.position; //Move player to checkpoint location
 
